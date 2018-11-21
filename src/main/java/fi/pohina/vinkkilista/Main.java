300acbef0f1eb16b99c6196970c3c52b3ab5910a
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-11-22 01:20:55 +0200

    Integrate BookmarkDao to BookmarkService

--- src/main/java/fi/pohina/vinkkilista/Main.java
+++ src/main/java/fi/pohina/vinkkilista/Main.java
@@ -1,10 +1,15 @@
 package fi.pohina.vinkkilista;
 
+import fi.pohina.vinkkilista.data_access.BookmarkDao;
+import fi.pohina.vinkkilista.data_access.InMemoryBookmarkDao;
+import fi.pohina.vinkkilista.domain.Blog;
 import fi.pohina.vinkkilista.domain.BookmarkService;
 
 public class Main {
     public static void main(String[] args) {
-        BookmarkService service = new BookmarkService();
+        BookmarkDao dao = new InMemoryBookmarkDao();
+        addMockBlogs(dao);
+        BookmarkService service = new BookmarkService(dao);
         int port = getPort();
 
         new App(service).startServer(port);
@@ -13,5 +18,22 @@ public class Main {
     private static int getPort() {
         String port = System.getenv("PORT");
         return port == null ? 4567 : Integer.parseInt(port);
+    }
+
+    private static void addMockBlogs(BookmarkDao blogs) {
+        blogs.add(new Blog(
+            "GitHub Blog",
+            "https://blog.github.com",
+            "GitHub"
+        ));
+        blogs.add(new Blog(
+            "Domain Driven Design Weekly",
+            "http://dddweekly.com"
+        ));
+        blogs.add(new Blog(
+            "the morning paper",
+            "https://blog.acolyer.org",
+            "Adrian Colyer"
+        ));
     }
 }
