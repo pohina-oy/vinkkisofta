67b145dac57ffe2e2c16a3a799c4eb9a9b490a5f
jesushilden <peter.e.hilden@gmail.com>
2018-11-21 20:04:01 +0200

    validation and styling

--- src/main/java/fi/pohina/vinkkilista/App.java
+++ src/main/java/fi/pohina/vinkkilista/App.java
@@ -2,14 +2,17 @@ package fi.pohina.vinkkilista;
 
 import fi.pohina.vinkkilista.domain.Blog;
 import fi.pohina.vinkkilista.domain.BookmarkService;
+import fi.pohina.vinkkilista.domain.BookmarkType;
 import java.util.Collection;
 import spark.ModelAndView;
 import java.util.HashMap;
 import java.util.Map;
+import spark.QueryParamsMap;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
     private final BookmarkService bookmarks;
 
     public App(BookmarkService bookmarks) {
@@ -31,25 +34,51 @@ public class App {
             map.put("blogs", blogs);
             return render(map, "index");
         });
-        
+
         get("/new", (req, res) -> {
             Map<String, Object> map = new HashMap<>();
+            map.put("types", BookmarkType.values());
             return render(map, "new");
         });
-        
+
         post("/new", (req, res) -> {
             String type = req.queryParams("type");
-            
-            if(type.equals("blog")) {
-                String title = req.queryParams("title");
-                String url = req.queryParams("url");
-                String author = req.queryParams("author");
-                bookmarks.addBlog(new Blog(title, url, author));
+            boolean created = false;
+            if (type.equals("Blog")) {
+                created = newBlog(req.queryMap());
             }
-            
-            res.redirect("/");
-            return "New bookmark added";
+
+            if (created) {
+                res.redirect("/");
+                return "New bookmark added";
+            } else {
+                res.redirect("/new");
+                return "Bookmark could not be created";
+            }
         });
+    }
+
+    /**
+     * Validates parameters and creates blog.
+     * Todo: Make a validator class.
+     * @param params Contains the parameters from request.
+     * @return Returns true if blog is added, otherwise false.
+     */
+    private boolean newBlog(QueryParamsMap params) {
+        String title = params.get("title").value();
+        String url = params.get("url").value();
+        String author = params.get("author").value();
+
+        if (title.isEmpty() || url.isEmpty()) {
+            return false;
+        }
+
+        if (author.isEmpty()) {
+            bookmarks.addBlog(new Blog(title, url));
+        } else {
+            bookmarks.addBlog(new Blog(title, url, author));
+        }
+        return true;
     }
 
     /**
@@ -60,11 +89,11 @@ public class App {
     }
 
     private static String render(
-        Map<String, Object> model,
-        String templatePath
+            Map<String, Object> model,
+            String templatePath
     ) {
         return new ThymeleafTemplateEngine().render(
-            new ModelAndView(model, templatePath)
+                new ModelAndView(model, templatePath)
         );
     }
 }
