bb20808664c8115f380ff68508ae1eb5bdc1e7a3
l-y-l <yilin.li@helsinki.fi>
2018-11-27 14:00:16 +0200

    added empty tests

--- src/test/java/fi/pohina/vinkkilista/domain/BookmarkServiceTest.java
+++ src/test/java/fi/pohina/vinkkilista/domain/BookmarkServiceTest.java
@@ -4,6 +4,7 @@ import fi.pohina.vinkkilista.data_access.BookmarkDao;
 import fi.pohina.vinkkilista.data_access.InMemoryBookmarkDao;
 import fi.pohina.vinkkilista.data_access.TagDao;
 import fi.pohina.vinkkilista.data_access.InMemoryTagDao;
+import java.util.*;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -12,18 +13,19 @@ import static org.mockito.Mockito.*;
 public class BookmarkServiceTest {
 
     private BookmarkService bookmarks;
+    private BookmarkDao bookmarkDao;
+    private TagDao tagDao;
 
     @Before
     public void setUp() {
+        bookmarkDao = spy(new InMemoryBookmarkDao());
+        tagDao = spy(new InMemoryTagDao());
+
+        bookmarks = new BookmarkService(bookmarkDao, tagDao);
     }
 
     @Test
     public void createBookmarkCreatesCorrectBookmark() {
-        BookmarkDao bookmarkDao = spy(new InMemoryBookmarkDao());
-        TagDao tagDao = spy(new InMemoryTagDao());
-
-        bookmarks = new BookmarkService(bookmarkDao, tagDao);
-
         String title = "foobar",
             url = "http://foo.com",
             author = "author";
@@ -44,6 +46,10 @@ public class BookmarkServiceTest {
         assertEquals(author, createdBookmark.getAuthor());
     }
 
+    @Test
+    public void createTagCreatesCorrectTag() {
+    }
+
     /**
      * Tests that searching for bookmarks by tags returns a list of only
      * matching bookmarks
@@ -58,5 +64,17 @@ public class BookmarkServiceTest {
      */
     @Test
     public void searchBookmarksByTagsReturnsEmptyListIfNoMatches() {
+    }
+
+    @Test
+    public void parseTagsFromStringCorrectlyParsesString() {
+    }
+
+    @Test
+    public void tagSetStringToObjectFindsExistingTags() {
+    }
+
+    @Test
+    public void tagSetStringToObjectCreatesMissingTags() {
     }
 }
