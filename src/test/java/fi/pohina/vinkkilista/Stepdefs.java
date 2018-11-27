ab071da5740af05f663111eb98c2d9cf526d0d06
jesushilden <peter.e.hilden@gmail.com>
2018-11-27 10:12:47 +0200

    selenium tests for bookmark form

--- src/test/java/fi/pohina/vinkkilista/Stepdefs.java
+++ src/test/java/fi/pohina/vinkkilista/Stepdefs.java
@@ -1,57 +1,87 @@
 package fi.pohina.vinkkilista;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Stepdefs {
 
+    private WebDriver driver = new ChromeDriver();
+    private String baseUrl = "http://localhost:4567/";
+
     @Given("new bookmark is selected")
     public void new_bookmark_is_selected() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
-
-    @Given("type is set to blog")
-    public void type_is_set_to_blog() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.id("newBookmarkLink"));
+        element.click();
     }
 
     @When("valid title {string} and valid url {string} are given")
-    public void valid_title_and_valid_url_are_given(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
-
-    @Then("a new blog is created")
-    public void a_new_blog_is_created() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void valid_title_and_valid_url_are_given(String title, String url) {
+        WebElement element = driver.findElement(By.id("titleInput"));
+        element.sendKeys(title);
+        element = driver.findElement(By.id("urlInput"));
+        element.sendKeys(url);
+        element = driver.findElement(By.id("submitForm"));
+        element.submit();
     }
 
     @When("valid title {string} and valid url {string} and valid author {string} are given")
-    public void valid_title_and_valid_url_and_valid_author_are_given(String string, String string2, String string3) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void valid_title_and_valid_url_and_valid_author_are_given(String title, String url, String author) {
+        WebElement element = driver.findElement(By.id("titleInput"));
+        element.sendKeys(title);
+        element = driver.findElement(By.id("urlInput"));
+        element.sendKeys(url);
+        element = driver.findElement(By.id("authorInput"));
+        element.sendKeys(author);
+        element = driver.findElement(By.id("submitForm"));
+        element.submit();
     }
 
     @When("missing title {string} and valid url {string} are given")
-    public void missing_title_and_valid_url_are_given(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
-
-    @Then("a new blog is not created")
-    public void a_new_blog_is_not_created() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void missing_title_and_valid_url_are_given(String title, String url) {
+        WebElement element = driver.findElement(By.id("titleInput"));
+        element.sendKeys(title);
+        element = driver.findElement(By.id("urlInput"));
+        element.sendKeys(url);
+        element = driver.findElement(By.id("submitForm"));
+        element.submit();
     }
 
     @When("valid title {string} and missing url {string} are given")
-    public void valid_title_and_missing_url_are_given(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void valid_title_and_missing_url_are_given(String title, String url) {
+        WebElement element = driver.findElement(By.id("titleInput"));
+        element.sendKeys(title);
+        element = driver.findElement(By.id("urlInput"));
+        element.sendKeys(url);
+        element = driver.findElement(By.id("submitForm"));
+        element.submit();
+    }
+
+    @Then("a new bookmark is created")
+    public void a_new_bookmark_is_created() {
+        pageUrlIs(baseUrl);
+    }
+
+    @Then("a new bookmark is not created")
+    public void a_new_bookmark_is_not_created() {
+        pageUrlIs(baseUrl + "new");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private void pageUrlIs(String url) {
+        assertEquals(driver.getCurrentUrl(), url);
+    }
+
+    private void pageHasContent(String content) {
+        assertTrue(driver.getPageSource().contains(content));
     }
 }
--- src/test/resources/fi/pohina/vinkkilista/create_bookmark.feature
+++ src/test/resources/fi/pohina/vinkkilista/create_bookmark.feature
@@ -1,25 +1,21 @@
 Feature: A bookmark can be created with valid inputs
 
-  Scenario: blog is created with valid title and url
+  Scenario: bookmark is created with valid title and url
     Given new bookmark is selected
-    And type is set to blog
-    When valid title "uusi" and valid url "http://www.somesite.org" are given
-    Then a new blog is created
+    When valid title "testBookmark" and valid url "http://www.somesite.org" are given
+    Then a new bookmark is created
 
-  Scenario: blog is created with valid title, url and author
+  Scenario: bookmark is created with valid title, url and author
     Given new bookmark is selected
-    And type is set to blog
-    When valid title "uusi" and valid url "http://www.somesite.org" and valid author "testimies" are given
-    Then a new blog is created
+    When valid title "testBookmark2" and valid url "http://www.somesite.org" and valid author "testAuthor" are given
+    Then a new bookmark is created
 
-  Scenario: blog is not created with missing title
+  Scenario: bookmark is not created with missing title
     Given new bookmark is selected
-    And type is set to blog
     When missing title "" and valid url "http://www.somesite.org" are given
-    Then a new blog is not created
+    Then a new bookmark is not created
 
-  Scenario: blog is not created with missing url
+  Scenario: bookmark is not created with missing url
     Given new bookmark is selected
-    And type is set to blog
-    When valid title "uusi" and missing url "" are given
-    Then a new blog is not created
+    When valid title "testBookmark3" and missing url "" are given
+    Then a new bookmark is not created
