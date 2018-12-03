36c3161b4f400ab78dc70d61b2455499b2fc115b
l-y-l <yilin.li@helsinki.fi>
2018-12-03 23:08:49 +0200

    implemented PostgresTagDao, cleaned PostgresBookmarkDao

--- src/main/java/fi/pohina/vinkkilista/data_access/PostgresBookmarkDao.java
+++ src/main/java/fi/pohina/vinkkilista/data_access/PostgresBookmarkDao.java
@@ -3,27 +3,18 @@ package fi.pohina.vinkkilista.data_access;
 import fi.pohina.vinkkilista.domain.Bookmark;
 import fi.pohina.vinkkilista.domain.Tag;
 
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.sql.*;
+import java.util.*;
 
 /**
  * Provides an PostgreSQL implementation of the {@link BookmarkDao} interface,
  * backed by a {@link List<Bookmark>}.
  */
-public class PostgreBookmarkDao implements BookmarkDao {
+public class PostgresBookmarkDao implements BookmarkDao {
     private Connection db;
 
-    public PostgreBookmarkDao(String dbHost, String dbUser, String dbPassowrd, String dbName) {
-        String url = "jdbc:postgresql://" + dbHost + "/" + dbName + "?user=" + dbUser + "&password=" + dbPassowrd;
+    public PostgresBookmarkDao(String dbHost, String dbUser, String dbPassword, String dbName) {
+        String url = "jdbc:postgresql://" + dbHost + "/" + dbName + "?user=" + dbUser + "&password=" + dbPassword;
         try { 
             this.db = DriverManager.getConnection(url);
         } catch (Exception e) {
@@ -46,7 +37,8 @@ public class PostgreBookmarkDao implements BookmarkDao {
                 String title = rs.getString("title");
                 String url = rs.getString("url");
                 String author = rs.getString("author");
-                return new Bookmark(id, title, url, author);
+                Set<Tag> tags = findBookmarkTags(id);
+                return new Bookmark(id, title, url, author, tags);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -54,9 +46,12 @@ public class PostgreBookmarkDao implements BookmarkDao {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Bookmark> findAll() {
-        List<Bookmark> bookmarks = new ArrayList<Bookmark>();
+        List<Bookmark> bookmarks = new ArrayList<>();
         try {
             Statement st = this.db.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM bookmarks");
@@ -81,65 +76,24 @@ public class PostgreBookmarkDao implements BookmarkDao {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void add(Bookmark bookmark) {
         try {
             String query = "INSERT INTO bookmarks (id, title, url, author) " + " values (?, ? ,? ,?)";
             PreparedStatement st = this.db.prepareStatement(query);
-            String bookmarkId = UUID.randomUUID().toString();
-            st.setString(1, bookmarkId);
+            st.setString(1, bookmark.getId());
             st.setString(2, bookmark.getTitle());
             st.setString(3, bookmark.getUrl());
             st.setString(4, bookmark.getAuthor());
-            st.executeUpdate();
             for (Tag t : bookmark.getTags()) {
-                addBookmarkTag(t.getName(), bookmarkId);
+                addBookmarkTag(bookmark.getId(), t.getId());
             }
-            st.executeQuery();
+            st.executeUpdate();
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-    }
-
-    /**
-     * Gets a tag by name
-     * @param tagName
-     * @return
-     */
-    private Tag getTag(String tagName) {
-        try {
-            String query = "SELECT * FROM tags where name = ?";
-            PreparedStatement st = this.db.prepareStatement(query);
-            st.setString(1, tagName);
-            ResultSet rs = st.executeQuery();
-            if (rs.first()) {
-                String tagId = rs.getString("id");
-                return new Tag(tagId, tagName);
-            }
-            return null;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-    }
-
-    /**
-     * Creates a tag
-     * @param tagName
-     * @return
-     */
-    private String createTag(String tagName) {
-        try {
-            String query = "INSERT INTO tags (id, name) values (?, ?)";
-            PreparedStatement st = this.db.prepareStatement(query);
-            String tagId = UUID.randomUUID().toString();
-            st.setString(1, tagId);
-            st.setString(2, tagName);
-            st.execute();
-            return tagId;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
         }
     }
 
@@ -148,22 +102,13 @@ public class PostgreBookmarkDao implements BookmarkDao {
      * @param tagName
      * @param bookmarkId
      */
-    private void addBookmarkTag(String tagName, String bookmarkId) {
-        Tag tag = getTag(tagName);
+    private void addBookmarkTag(String bookmarkId, String tagId) {
         String query = "INSERT INTO bookmark_tags (\"bookmarkId\", \"tagId\") values (?, ?)";
         try {
-            if (tag != null) {
-                PreparedStatement st = this.db.prepareStatement(query);
-                st.setString(1, bookmarkId);
-                st.setString(2, tag.getId());
-                st.executeQuery();
-            } else {
-                String tagId = createTag(tagName);
-                PreparedStatement st = this.db.prepareStatement(query);
-                st.setString(1, bookmarkId);
-                st.setString(2, tagId);
-                st.executeQuery();
-            }
+            PreparedStatement st = this.db.prepareStatement(query);
+            st.setString(1, bookmarkId);
+            st.setString(2, tagId);
+            st.executeUpdate();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -195,7 +140,7 @@ public class PostgreBookmarkDao implements BookmarkDao {
         for (String tag : tagSet) {
             tagArray.add(tag);
         }
-        String query = "select bookmarks.* from bookmark_tags inner join bookmarks on bookmark_tags.\"bookmarkId\" = bookmarks.id inner join tags on  tags.id = bookmark_tags.\"tagId\"  where tags.name = ANY(?)";
+        String query = "select bookmarks.* from bookmark_tags inner join bookmarks on bookmark_tags.\"bookmarkId\" = bookmarks.id inner join tags on tags.id = bookmark_tags.\"tagId\" where tags.name = ANY(?)";
         try {
             PreparedStatement st = this.db.prepareStatement(query);
             Array array = this.db.createArrayOf("varchar", tagArray.toArray());
---
+++ src/main/java/fi/pohina/vinkkilista/data_access/PostgresTagDao.java
@@ -0,0 +1,105 @@
+package fi.pohina.vinkkilista.data_access;
+
+import fi.pohina.vinkkilista.domain.Tag;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * Provides an PostgreSQL implementation of the {@link TagDao} interface,
+ * backed by a {@link List<Tag>}.
+ */
+public class PostgresTagDao implements TagDao {
+    private Connection db;
+
+    public PostgresTagDao(String dbHost, String dbUser, String dbPassword, String dbName) {
+        String url = "jdbc:postgresql://" + dbHost + "/" + dbName + "?user=" + dbUser + "&password=" + dbPassword;
+        try { 
+            this.db = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * This is a linear time operation.
+     */
+    @Override
+    public Tag findById(String id) {
+        try {
+            String query = "SELECT * FROM tags WHERE id = ?";
+            PreparedStatement st = this.db.prepareStatement(query);
+            st.setString(1, id);
+            ResultSet rs = st.executeQuery();
+            if (rs.first()) {
+                String name = rs.getString("name");
+                return new Tag(id, name);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tag findByName(String name) {
+        try {
+            String query = "SELECT * FROM tags WHERE name = ?";
+            PreparedStatement st = this.db.prepareStatement(
+                query,
+                ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.CONCUR_UPDATABLE
+            );
+            st.setString(1, name);
+            ResultSet rs = st.executeQuery();
+            if (rs.first()) {
+                String id = rs.getString("id");
+                return new Tag(id, name);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Tag> findAll() {
+        List<Tag> tags = new ArrayList<>();
+        try {
+            Statement st = this.db.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM tags");
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                tags.add(new Tag(id, name));                
+            }
+            return tags;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void add(Tag tag) {
+        try {
+            String query = "INSERT INTO tags (id, name) " + " values (?, ?)";
+            PreparedStatement st = this.db.prepareStatement(query);
+            st.setString(1, tag.getId());
+            st.setString(2, tag.getName());
+            st.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
