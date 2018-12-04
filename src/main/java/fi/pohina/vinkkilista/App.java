79264f9fe71a61767b515fc925b224734704185a
SirVeggie <sirveggie@hotmail.com>
2018-12-05 01:25:39 +0200

    Update App.java

--- src/main/java/fi/pohina/vinkkilista/App.java
+++ src/main/java/fi/pohina/vinkkilista/App.java
@@ -125,9 +125,6 @@ public class App {
         });
 
         get("/auth/gh-callback", (req, res) -> {
-            System.out.println("App::githubCallback");
-            System.out.println("Github variables:\nGITHUB_CLIENT_ID: " + GITHUB_CLIENT_ID + "\nGITHUB_LOGIN_SECRET: " + GITHUB_CLIENT_SECRET);
-
             String callbackCode = req.queryParams("code");
             String url = "https://github.com/login/oauth/access_token";
 
@@ -148,9 +145,8 @@ public class App {
 
             // 1.1 print access token response keys
 
-            System.out.println("  - POST /login/oauth/access_token");
             List<NameValuePair> responseQuery = URLEncodedUtils.parse(responseEntity);
-            responseQuery.forEach(nvp -> System.out.println("    * " + nvp.getName() + " - " + nvp.getValue()));
+            //responseQuery.forEach(nvp -> System.out.println("    * " + nvp.getName() + " - " + nvp.getValue()));
 
             String accessToken = responseQuery
                     .stream()
@@ -168,45 +164,20 @@ public class App {
                             .returnContent()
                             .asString();
 
-            System.out.println("  - GET /user\n     * " + userInfoJson);
-
             GithubUser githubUser = new Gson().fromJson(userInfoJson, GithubUser.class);
 
-            if (githubUser.getEmail() == null) {
-                System.out.println("  - Email was null, fetching all emails");
-                // 3. get user emails because if user has multiple emails, we need to find the primary one
-                String userEmailsJson =
-                        org.apache.http.client.fluent.Request.Get("https://api.github.com/user/emails")
-                            .addHeader("Authorization", String.format("Bearer %s", accessToken))
-                            .addHeader("Accept", "application/json")
-                            .execute()
-                            .returnContent()
-                            .asString();
 
-                System.out.println("  - GET /user/emails\n     * " + userEmailsJson);
+            // 3. get user emails because if user has multiple emails, we need to find the primary one
+            if (githubUser.getEmail() == null)
+                githubUser.setEmail(getPrimaryUserEmail(accessToken));
 
-                Type emailListType = new TypeToken<List<GithubEmail>>() {
-                }.getType();
 
-                List<GithubEmail> userEmails = new Gson().fromJson(userEmailsJson, emailListType);
 
-                GithubEmail primaryEmail = userEmails.stream()
-                            .filter(email -> email.isPrimary())
-                            .findFirst()
-                            .orElse(userEmails.get(0));
-
-                System.out.println("  - Identified primary email as: " + primaryEmail.getEmail());
-                githubUser.setEmail(primaryEmail.getEmail());
-            }
-
-            System.out.println("  - Finding or creating user by Github user");
             User user = users.findOrCreateByGithubUser(githubUser);
 
-            System.out.println("  - user: " + user);
             req.session(true)
                     .attribute(SESSION_ATTRIBUTE_USERID, user.getId());
 
-            System.out.println("  - Finish, redirect to /bookmarks/");
             res.redirect("/bookmarks/");
             return "";
         });
@@ -229,6 +200,28 @@ public class App {
 
     private String getUserIdFromSession(Session session) {
         return session.attribute(SESSION_ATTRIBUTE_USERID);
+    }
+
+    private String getPrimaryUserEmail(String accessToken) throws Exception {
+        String userEmailsJson =
+                org.apache.http.client.fluent.Request.Get("https://api.github.com/user/emails")
+                        .addHeader("Authorization", String.format("Bearer %s", accessToken))
+                        .addHeader("Accept", "application/json")
+                        .execute()
+                        .returnContent()
+                        .asString();
+
+        Type emailListType = new TypeToken<List<GithubEmail>>() {
+        }.getType();
+
+        List<GithubEmail> userEmails = new Gson().fromJson(userEmailsJson, emailListType);
+
+        GithubEmail primaryEmail = userEmails.stream()
+                .filter(email -> email.isPrimary())
+                .findFirst()
+                .orElse(userEmails.get(0));
+
+        return primaryEmail.getEmail();
     }
 
     /**
