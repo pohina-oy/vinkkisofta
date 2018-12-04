1bca245c1293b28366258108353cb8a7c96c6fcb
Hugo Holmqvist <hugo.holm@live.com>
2018-12-04 17:13:21 +0200

    Fix checkstyle

--- src/main/java/fi/pohina/vinkkilista/domain/Bookmark.java
+++ src/main/java/fi/pohina/vinkkilista/domain/Bookmark.java
@@ -53,6 +53,19 @@ public class Bookmark {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bookmark bookmark = (Bookmark) o;
+        return Objects.equals(id, bookmark.id)
+            && Objects.equals(title, bookmark.title);
+    }
+
+    @Override
     public int hashCode() {
         return id.hashCode();
     }
