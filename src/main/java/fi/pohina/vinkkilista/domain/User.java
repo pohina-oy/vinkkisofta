dc535e75c964657fa0594e69aa524f65c6b9f0d5
l-y-l <yilin.li@helsinki.fi>
2018-12-05 22:38:07 +0200

    added HashMap private to User with getters and setters

--- src/main/java/fi/pohina/vinkkilista/domain/User.java
+++ src/main/java/fi/pohina/vinkkilista/domain/User.java
@@ -1,17 +1,21 @@
 package fi.pohina.vinkkilista.domain;
 
+import java.util.HashMap;
+
 public class User {
 
     private final String id;
     private final String email;
     private final String username;
     private final int githubId;
+    private final HashMap<String, String> bookmarkReadDates;
 
     public User(String id, String email, String username, int githubId) {
         this.id = id;
         this.email = email;
         this.username = username;
         this.githubId = githubId;
+        bookmarkReadDates = new HashMap<>();
     }
 
     public String getId() {
@@ -28,5 +32,21 @@ public class User {
 
     public int getGithubId() {
         return githubId;
+    }
+
+    public String getBookmarkReadStatus(String bookmarkId) {
+        if (bookmarkReadDates.containsKey(bookmarkId)) {
+            return bookmarkReadDates.get(bookmarkId);
+        }
+
+        return null;
+    }
+
+    public void setBookmarkReadStatus(String bookmarkId, String dateRead) {
+        bookmarkReadDates.put(bookmarkId, dateRead);
+    }
+
+    public void removeBookmarkReadStatus(String bookmarkId) {
+        bookmarkReadDates.remove(bookmarkId);
     }
 }
