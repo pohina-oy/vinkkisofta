9d5c60f114895dd9530baafe2f11025ea7e89e4c
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-12-08 01:16:21 +0200

    Make RequestUserManager package-private, document

--- src/main/java/fi/pohina/vinkkilista/RequestUserManager.java
+++ src/main/java/fi/pohina/vinkkilista/RequestUserManager.java
@@ -5,17 +5,29 @@ import fi.pohina.vinkkilista.domain.UserService;
 import spark.Request;
 import spark.Session;
 
-public class RequestUserManager {
+/**
+ * Provides user login status management with sessions. Essentially extension
+ * methods to use with {@link Request Requests}.
+ */
+class RequestUserManager {
 
     private static final String SESSION_ATTRIBUTE_NAME_USERID = "github-user";
 
     private final UserService userService;
 
-    public RequestUserManager(UserService userService) {
+    RequestUserManager(UserService userService) {
         this.userService = userService;
     }
 
-    public User getSignedInUser(Request req) {
+    /**
+     * Retrieves the logged in user associated with the request. This method
+     * fetches the user's ID from the request's session (cookies), then finds
+     * the user from the {@link UserService}.
+     *
+     * @param req the Spark request whose user to retrieve.
+     * @return the signed in {@link User} making the request, or <c>null</c>.
+     */
+    User getSignedInUser(Request req) {
         String userId = getUserIdFromCookie(req.session());
         if (userId == null) {
             return null;
@@ -23,7 +35,14 @@ public class RequestUserManager {
         return userService.findById(userId);
     }
 
-    public void setSignedInUser(Request req, User user) {
+    /**
+     * Associates the specified {@link User} to the request by assigning
+     * the user's ID to the request's session (cookie).
+     *
+     * @param req  the request with which the session will be created.
+     * @param user the user to sign in.
+     */
+    void setSignedInUser(Request req, User user) {
         Session createdSession = req.session(true);
         setUserIdToCookie(createdSession, user.getId());
     }
