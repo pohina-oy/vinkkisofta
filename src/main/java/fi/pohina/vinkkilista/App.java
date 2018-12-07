c02db5b9ee59bc2f290625016b2e59d128eee571
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-12-07 18:23:44 +0200

    Extract Github JSON API to its own class

--- src/main/java/fi/pohina/vinkkilista/App.java
+++ src/main/java/fi/pohina/vinkkilista/App.java
@@ -1,8 +1,6 @@
 package fi.pohina.vinkkilista;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import fi.pohina.vinkkilista.api.GithubEmail;
+import fi.pohina.vinkkilista.api.GithubJsonApi;
 import fi.pohina.vinkkilista.api.GithubOAuthApi;
 import fi.pohina.vinkkilista.api.GithubUser;
 import fi.pohina.vinkkilista.domain.Bookmark;
@@ -13,7 +11,6 @@ import spark.*;
 import spark.ModelAndView;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import java.lang.reflect.Type;
 import java.util.*;
 
 import static spark.Spark.*;
@@ -123,16 +120,8 @@ public class App {
 
             String accessToken
                 = githubOAuthApi.exchangeCodeForAccessToken(callbackCode);
-
-            // 2. get user info
-            String userInfoJson = getUserInfoJson(accessToken);
-
-            GithubUser githubUser = new Gson().fromJson(userInfoJson, GithubUser.class);
-
-            // 3. get user emails because if user has multiple emails, we need to find the primary one
-            if (githubUser.getEmail() == null) {
-                githubUser.setEmail(getPrimaryUserEmail(accessToken));
-            }
+            GithubUser githubUser
+                = new GithubJsonApi(accessToken).getGithubUser();
 
             User user = users.findOrCreateByGithubUser(githubUser);
 
@@ -159,40 +148,6 @@ public class App {
 
     private String getUserIdFromSession(Session session) {
         return session.attribute(SESSION_ATTRIBUTE_USERID);
-    }
-
-    private String getUserInfoJson(String accessToken) throws Exception {
-        String userInfoJson
-                = org.apache.http.client.fluent.Request.Get("https://api.github.com/user")
-                        .addHeader("Authorization", String.format("Bearer %s", accessToken))
-                        .addHeader("Accept", "application/json")
-                        .execute()
-                        .returnContent()
-                        .asString();
-
-        return userInfoJson;
-    }
-
-    private String getPrimaryUserEmail(String accessToken) throws Exception {
-        String userEmailsJson
-                = org.apache.http.client.fluent.Request.Get("https://api.github.com/user/emails")
-                        .addHeader("Authorization", String.format("Bearer %s", accessToken))
-                        .addHeader("Accept", "application/json")
-                        .execute()
-                        .returnContent()
-                        .asString();
-
-        Type emailListType = new TypeToken<List<GithubEmail>>() {
-        }.getType();
-
-        List<GithubEmail> userEmails = new Gson().fromJson(userEmailsJson, emailListType);
-
-        GithubEmail primaryEmail = userEmails.stream()
-                .filter(email -> email.isPrimary())
-                .findFirst()
-                .orElse(userEmails.get(0));
-
-        return primaryEmail.getEmail();
     }
 
     /**
