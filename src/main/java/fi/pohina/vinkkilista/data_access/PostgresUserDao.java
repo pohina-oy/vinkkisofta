7b68d001ca8621303e0706bd191f1de84a6d6fc5
Hugo Holmqvist <hugo.holm@live.com>
2018-12-12 15:32:04 +0200

    Various fixes

--- src/main/java/fi/pohina/vinkkilista/data_access/PostgresUserDao.java
+++ src/main/java/fi/pohina/vinkkilista/data_access/PostgresUserDao.java
@@ -3,10 +3,9 @@ package fi.pohina.vinkkilista.data_access;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.time.LocalDateTime;
 
 import fi.pohina.vinkkilista.domain.User;
 import javax.sql.DataSource;
@@ -21,12 +20,11 @@ public class PostgresUserDao implements UserDao {
 
     @Override
     public User findById(String id) {
-        String query = "SELECT * FROM users left join user_read_bookmarks on users.id = user_read_bookmakrs.\"userId\" where users.id = ?";
+        String query = "SELECT * FROM users left join user_read_bookmarks on users.id = user_read_bookmarks.\"userId\" where users.id = ?";
         try (Connection conn = dataSource.getConnection()) {
             PreparedStatement st = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             st.setString(1, id);
             ResultSet rs = st.executeQuery();
-            User user;
             if (rs.first()) {
                 String userId = rs.getString("id");
                 String userEmail = rs.getString("email");
@@ -34,17 +32,17 @@ public class PostgresUserDao implements UserDao {
                 Integer userGithubId = rs.getInt("githubId");
 
                 String readBookmarkId = rs.getString("bookmarkId");
-                LocalDateTime readDate = rs.getTimestamp("readDate").toLocalDateTime();
-                user = new User(userId, userEmail, username, userGithubId);
-                user.setBookmarkReadStatus(readBookmarkId, readDate);
+                Timestamp readDate = rs.getTimestamp("readDate");
+                User user = new User(userId, userEmail, username, userGithubId);
+                user.setBookmarkReadStatus(readBookmarkId, readDate != null ? readDate.toLocalDateTime() : null);
+                while (rs.next()) {
+                    readBookmarkId = rs.getString("bookmarkId");
+                    readDate = rs.getTimestamp("readDate");
+                    user.setBookmarkReadStatus(readBookmarkId, readDate != null ? readDate.toLocalDateTime() : null);
+                }
+                return user;
             } else {
                 return null;
-            }
-
-            while (rs.next()) {
-                String readBookmarkId = rs.getString("bookmarkId");
-                LocalDateTime readDate = rs.getTimestamp("readDate").toLocalDateTime();
-                user.setBookmarkReadStatus(readBookmarkId, readDate);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -54,7 +52,7 @@ public class PostgresUserDao implements UserDao {
 
     @Override
     public User findByGithubId(int githubId) {
-        String query = "SELECT * FROM users where \"githubId\" = ?";
+        String query = "SELECT * FROM users left join user_read_bookmarks on users.id = user_read_bookmarks.\"userId\" where \"githubId\" = ?";
         try (Connection conn = dataSource.getConnection()) {
             PreparedStatement st = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             st.setInt(1, githubId);
@@ -64,7 +62,19 @@ public class PostgresUserDao implements UserDao {
                 String userEmail = rs.getString("email");
                 String username = rs.getString("username");
                 Integer userGithubId = rs.getInt("githubId");
-                return new User(userId, userEmail, username, userGithubId);
+
+                String readBookmarkId = rs.getString("bookmarkId");
+                Timestamp readDate = rs.getTimestamp("readDate");
+                User user = new User(userId, userEmail, username, userGithubId);
+                user.setBookmarkReadStatus(readBookmarkId, readDate != null ? readDate.toLocalDateTime() : null);
+                while (rs.next()) {
+                    readBookmarkId = rs.getString("bookmarkId");
+                    readDate = rs.getTimestamp("readDate");
+                    user.setBookmarkReadStatus(readBookmarkId, readDate != null ? readDate.toLocalDateTime() : null);
+                }
+                return user;
+            } else {
+                return null;
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -94,11 +104,28 @@ public class PostgresUserDao implements UserDao {
 
     @Override
     public void addBookmarkReadDate(String userId, String bookmarkId, LocalDateTime dateRead) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "INSERT INTO user_read_bookmarks (\"userId\",\"bookmarkId\",\"readDate\") values(?,?,?)";
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setString(1, userId);
+            st.setString(2, bookmarkId);
+            st.setTimestamp(3, Timestamp.valueOf(dateRead));
+            st.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void removeBookmarkReadDate(String userId, String bookmarkId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "DELETE FROM user_read_bookmarks WHERE \"userId\" = ? AND \"bookmarkId\" = ?";
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setString(1, userId);
+            st.setString(2, bookmarkId);
+            st.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
